72debcfb7f6977111dab978a7e9d7a36e67658e6
Adesina Adebisi <104420789+AdebisiRex@users.noreply.github.com>
2023-10-18 11:32:17 +0100

    added createdAt to  visitors

--- models/visitors.model.js
+++ models/visitors.model.js
@@ -5,6 +5,7 @@ const visitorSchema = mongoose.Schema({
   lastName: { type: String, required: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now() },
 });
 
 const visitorModel = mongoose.model("visitor", visitorSchema);
